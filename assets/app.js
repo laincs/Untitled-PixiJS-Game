3046e7947efe204d10e33eee138843048baf43c3
Liân <42004259+laincs@users.noreply.github.com>
2025-03-11 20:10:25 -0300

    Init

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+const app = new PIXI.Application();
+await app.init({ width: 1000, height: 500 })
+document.body.appendChild(app.canvas);
+
+await PIXI.Assets.load('/assets/images/sample.png');
+let sprite = PIXI.Sprite.from('/assets/images/sample.png');
+app.stage.addChild(sprite);
+
+let elapsed = 0.0;
+
+app.ticker.add((ticker) => {
+    elapsed += ticker.deltaTime;
+    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
